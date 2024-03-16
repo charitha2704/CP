b2867e9ef4d329de2f88c88b93934804bc504c5f
charitha2704 <157295317+charitha2704@users.noreply.github.com>
2024-03-16 19:28:42 +0530

    Create Monotone Increasing Digits.py

---
+++ Monotone Increasing Digits.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def monotoneIncreasingDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        listNum = list(str(n))
+        flag=len(listNum)
+
+        for i in range(len(listNum)-1,0,-1):
+            if listNum[i-1]>listNum[i]:
+                flag=i
+                listNum[i-1]=str(int(listNum[i-1])-1)
+
+        for i in range(flag,len(listNum)):
+            listNum[i]='9'
+        
+        return int("".join(listNum))
