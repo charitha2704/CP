b6208296404b414ffa0b8afa93a58cd7aca93422
charitha2704 <157295317+charitha2704@users.noreply.github.com>
2024-03-26 15:16:00 +0530

    Create Rotate List.py

---
+++ Rotate List.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        stack=[]
+        while head:
+            stack.append(head)
+            head=head.next
+        l=len(stack)
+        k=k%l
+        if k!=l:
+            stack=stack[-k:]+stack[:l-k]
+        i=0
+        head=ListNode()
+        tmp=head
+        while i<l:
+            head.next=stack[i]
+            head=head.next
+            i+=1
+        head.next=None
+        return tmp.next
